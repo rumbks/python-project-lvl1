1a862e43f39965f8ef35d89133686e19ff7189b3
aukhadeev <rumbks@gmail.com>
2021-05-25 15:54:46 +0500

    Implement progression game

---
+++ brain_games/game/progression.py
@@ -0,0 +1,42 @@
+from random import choice, randint
+from typing import List
+
+from brain_games.game import Round
+
+
+def show_rules() -> None:
+    print('What number is missing in the progression?')
+
+
+def get_progression(initial_term: int, difference: int, length: int) -> List[int]:
+    progression = []
+    item = initial_term
+    for _ in range(length):
+        progression.append(item)
+        item += difference
+    return progression
+
+
+def get_representation_with_hidden_item(progression: List[int], hidden_item_index: int) -> str:
+    items_strings = [str(item) for item in progression]
+    items_strings[hidden_item_index] = ".."
+    return " ".join(items_strings)
+
+
+def get_round() -> Round:
+    initial_term_upper_bound = 30
+    initial_term = randint(0, initial_term_upper_bound)
+
+    length_lower_bound, length_upper_bound = 5, 15
+    progression_length = randint(length_lower_bound, length_upper_bound)
+
+    difference_lower_bound, difference_upper_bound = 1, 10
+    progression_difference = randint(difference_lower_bound, difference_upper_bound)
+
+    progression = get_progression(initial_term, progression_difference, progression_length)
+    hidden_item_index = randint(0, progression_length - 1)
+
+    question = get_representation_with_hidden_item(progression, hidden_item_index)
+    hidden_item = progression[hidden_item_index]
+
+    return Round(question=question, answer=str(hidden_item))
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.game import progression as progression_game
+from brain_games.scripts import show_game_cli
+
+
+def main():
+    show_game_cli(progression_game)
+
+
+if __name__ == '__main__':
+    main()
