c367c237e284bdfb21195327009723857743cbcd
aukhadeev <rumbks@gmail.com>
2021-05-25 11:10:48 +0500

    Refactor brain_games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,13 @@
 import prompt
 
 
-def welcome_user():
-    name = prompt.string('May I have your name? ')
+def welcome() -> None:
+    print("Welcome to the Brain Games!")
+
+
+def get_user_name() -> str:
+    return prompt.string('May I have your name? ')
+
+
+def welcome_user(name: str) -> None:
     print(f"Hello, {name}!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from brain_games.cli import welcome_user
+from brain_games.cli import welcome_user, get_user_name, welcome
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    welcome_user()
+    welcome()
+    name = get_user_name()
+    welcome_user(name)
 
 
 if __name__ == '__main__':
