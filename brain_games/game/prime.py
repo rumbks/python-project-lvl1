23c7ee64664d48c703726214209ff89138d9ffff
aukhadeev <rumbks@gmail.com>
2021-05-25 16:08:10 +0500

    Implement prime game

---
+++ brain_games/game/prime.py
@@ -0,0 +1,28 @@
+from math import sqrt
+from random import choice, randint
+from operator import add, sub, mul
+from brain_games.game import Round
+
+
+def divides(a: int, b: int) -> bool:
+    return a % b == 0
+
+
+def is_prime(n) -> bool:
+    for i in range(2, int(sqrt(n))+1):
+        if divides(n, i):
+            return False
+    return True
+
+
+def show_rules() -> None:
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+
+def get_round() -> Round:
+    upper_bound = 100
+
+    number = randint(2, upper_bound)
+    answer = "yes" if is_prime(number) else "no"
+
+    return Round(question=str(number), answer=answer)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.game import prime as prime_game
+from brain_games.scripts import show_game_cli
+
+
+def main():
+    show_game_cli(prime_game)
+
+
+if __name__ == '__main__':
+    main()
